5038b2bf05a2e761a3e587901bd2f750fe5e75a2
guluehmedova <guliehmedova19@gmail.com>
2024-02-03 01:10:10 +0400

    first commit

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+document.getElementById('open_sidebar').addEventListener('click', function () {
+    document.getElementById('sidebar').style.width = "270px";
+    document.getElementById('wrapper').style.marginLeft = "270px";
+    document.getElementById('wrapper').style.overflow = "hidden";
+});
+
+document.getElementById('close_sidebar').addEventListener('click', function () {
+    document.getElementById('sidebar').style.width = "0";
+    document.getElementById('wrapper').style.marginLeft = "0";
+    document.getElementById('wrapper').style.overflow = "unset";
+});
